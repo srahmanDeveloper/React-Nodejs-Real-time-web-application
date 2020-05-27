3fe95770484e79f4217262f34643a0175141787a
srahmanDeveloper <56480997+srahmanDeveloper@users.noreply.github.com>
2020-05-27 13:02:31 +0100

    Add files via upload

--- reactSource/App.js
+++ reactSource/App.js
@@ -6,17 +6,74 @@ import Form from "./components/Form";
 import ContactUs from "./components/ContactUs";
 import ProductShop from "./components/ProductShop";
 
+class NavLink extends Component {
+
+  render() {
+      return (
+        <li className={"nav-item " + (this.props.isActive ? "active": "")}>
+
+          <h4 className="nav-link" onClick={() => this.props.onClick()}> 
+            {this.props.text}
+          </h4>
+              
+        </li>
+      );
+  }
+}
+
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      links: [
+        {path: "/", text: "Home", isActive: false},
+        {path: "/ProductShop", text: "Product Shop", isActive: false},
+        {path: "/Form", text: "Form", isActive: false},
+        {path: "/About", text: "About Us", isActive: false},
+        {path: "/BirdGallery", text: "Gallery", isActive: false},
+      ]
+    }
+  }
+
+  handleClick(i) {
+    
+    console.log(i);
+    console.log(this.state);
+    const links = this.state.links.slice(); 
+    for (const j in links) {
+      links[j].isActive = i == j ;
+      
+    }
+    this.setState({links: links});
+    location.href = links[i].path;
+  }
+
   render() {
     return (
+      
       <div className="App">
+	      
+	      <nav className="navbar navbar-expand-lg navbar-light  bg-light">
+          <ul className="navbar-nav">
+            {this.state.links.map((link, i) => 
+              < NavLink
+                path={link.path} 
+                text={link.text} 
+                isActive={link.isActive}
+                key={link.path} 
+                onClick={() => this.handleClick(i)}
+              /> 
+              )}
+          </ul>
+        </nav>
 	      <Router>
 		      <div>
-			      <Route exact path="/" component={Home} />
-            <Route exact path="/About" component={About} />
-            <Route exact path="/Form" component={Form} />
-            <Route exact path="/Contact" component={ContactUs} />
-            <Route exact path="/ProductShop" component={ProductShop} />
+		      	<Route exact path="/" component={Home} />
+	            <Route exact path="/About" component={About} />
+	            <Route exact path="/Form" component={Form} />
+	            <Route exact path="/Contact" component={ContactUs} />
+	            <Route exact path="/ProductShop" component={ProductShop} />
             
 		      </div>
 	      </Router>
