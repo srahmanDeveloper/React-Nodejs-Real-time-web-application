2a9c55598d2b7ab846be013a0e1883dbbf437f6f
srahmanDeveloper <56480997+srahmanDeveloper@users.noreply.github.com>
2020-06-06 13:04:20 +0100

    Add files via upload

--- reactSource/Components/ProductShop/index.jsx
+++ reactSource/Components/ProductShop/index.jsx
@@ -4,34 +4,34 @@ import { connect } from 'react-redux';
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
 
-const images = [
-  require('C:/Users/saif/ToyShop/assetSource/upload/1570967890024.png'),
-  require('C:/Users/saif/ToyShop/assetSource/upload/1570967968039.png'),
-  require('C:/Users/saif/ToyShop/assetSource/upload/1570967890024.png'),
-  require('C:/Users/saif/ToyShop/assetSource/upload/1570967968039.png')
-];
+
 
 class ProductShop extends React.Component {
 
   
   constructor(props) {
+
     super(props);
     this.state = {
       isOpen: false,
       photoIndex: 0,
+      isOld: true,
       messages: [{
         _id: "5da311928d7efb06d49ae6c8",
         ProductUniqueId: 1570967890024,
         ProductPrice: "20",
+        ProductType: "None",
         ProductOffer: "3 for 2",
         ProductDes: "Casio Nice Watch",
         src: require('C:/Users/saif/ToyShop/assetSource/upload/1570967890024.png'),
         thumb:"",
+        selectedImageSrc: '',
         caption: ""
       }]
     }
 
     this.getProductInformation();
+    this.deleteProduct = this.deleteProduct.bind(this);
   }
 
   getProductInformation(){
@@ -57,10 +57,11 @@ class ProductShop extends React.Component {
     for(var src = 0; src < response.length; src++){
 
       var ImgSrc1 = require('C:/Users/saif/ToyShop/assetSource/upload/1570967890024.png');
+
       if(!response[src].ProductUniqueId){
         response[src].ProductUniqueId = 1570968011856;
       }
-      var ImgSrc = 'C:/Users/saif/ToyShop/assetSource/upload/' + response[src].ProductUniqueId + '.png';
+      var ImgSrc = require('C:/Users/saif/ToyShop/assetSource/upload/' + response[src].ProductUniqueId + '.png');
       console.log(ImgSrc);
       
       response[src].thumb = ImgSrc;
@@ -76,9 +77,18 @@ class ProductShop extends React.Component {
 
   }
 
-  deleteProduct(product){
+  deleteProduct(event){
 
-    console.log(3);
+    console.log(event.target.dataset);
+    //console.log(product);
+    var id = event.target.dataset.param;
+
+    return axios.get('http://localhost:4000/product/deleteProduct/' + id)
+      .then(res => {
+        
+        console.log('Product Deleted');
+        
+      })
 
   }
 
@@ -91,34 +101,44 @@ class ProductShop extends React.Component {
 
   render() {
 
-    var  photoIndex = 0;
-    var  isOpen = false;
+    /*var  photoIndex = 0;
+    var  isOpen = false;*/
     return (
 
-      <div className="container-fluid">  
+      <div className="container-fluid">
+
+            
+        <div className="row sectionGap">
+
+            <div className="usedProductHighlighter"> <p className="productHighlighterText"> Used </p> </div>
+            <div className="newProductHighlighter"> <p className="productHighlighterText"> New </p>  </div>
+
+        </div>
 
         <div className="row sectionGap">
 
-          <div>
-
+          
+            
             {this.state.messages.map(product => (
               
               <div className="row" key={product._id}>
                 <div className="col-sm-10" className="panelMargin">
-                  <div>
+                  <div className={ product.ProductType == 'Used' ? 'productOldImageBorder' : 'productNewImageBorder' }>
                   
-                  <img src={require(`C:/Users/saif/ToyShop/assetSource/upload/${product.ProductUniqueId}.png`)} style={{height:200, width:200}} />
+                  <img src={require(`C:/Users/saif/ToyShop/assetSource/upload/${product.ProductUniqueId}.png`)} onClick={() => this.setState({ isOpen: true, selectedImageSrc: require(`C:/Users/saif/ToyShop/assetSource/upload/${product.ProductUniqueId}.png`) })} style={{height:200, width:200}} />
                   
                   <p> Description: {product.ProductDes} </p>
                   <p> Src: {product.src} </p>
                   <p> Price: £{product.ProductPrice} </p>
+                  <p> Type: {product.ProductType} </p>
                   <p> Offer: {product.ProductOffer} </p>
                   <p> Title: {product.ProductText} </p>
 
                   <input type="number" className="quantityHeight" id="quantity" placeholder="Qty" />
 
                   <button className="button btn btn-primary" onClick={this.addProduct}>Add To Basket </button>
-                  <button className="button btn btn-danger" onClick={this.deleteProduct}>Delete</button>
+                  <button data-param={product._id} className="button btn btn-danger" onClick={this.deleteProduct}>Delete</button>
+
                   <button className="button btn btn-success" onClick={this.editProduct}>Edit</button>
                 </div>
                 </div>
@@ -126,32 +146,18 @@ class ProductShop extends React.Component {
               
             ))}
 
-          </div>
-
-          <button type="button" style={{height:50}} onClick={() => this.setState({ isOpen: true })}>
-              Open Lightbox
-            </button>
-     
-            { /*isOpen &&*/ (
-              <Lightbox
-                mainSrc={images[photoIndex]}
-                nextSrc={images[(photoIndex + 1) % images.length]}
-                prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-                onCloseRequest={() => this.setState({ isOpen: false })}
-                onMovePrevRequest={() =>
-                  this.setState({
-                    photoIndex: (photoIndex + images.length - 1) % images.length,
-                  })
-                }
-                onMoveNextRequest={() =>
-                  this.setState({
-                    photoIndex: (photoIndex + 1) % images.length,
-                  })
-                }
-              />
-            )}
-
         </div>
+
+        <div className="row sectionGap">
+
+          { this.state.isOpen && (
+            <Lightbox
+              mainSrc={this.state.selectedImageSrc}
+              onCloseRequest={() => this.setState({ isOpen: false })}
+              
+            />
+          )}
+          </div>
 
       </div>
     )
