15212a7e2bcc030a4f3695aaa9ab7f020ac1ad03
srahmanDeveloper <56480997+srahmanDeveloper@users.noreply.github.com>
2020-06-06 19:11:29 +0100

    Created edit section

--- reactSource/Components/ProductShop/index.jsx
+++ reactSource/Components/ProductShop/index.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
+import Button from 'react-bootstrap/Button';
+import { Col, Row, Form } from "react-bootstrap";
 
 
 
@@ -79,8 +81,6 @@ class ProductShop extends React.Component {
 
   deleteProduct(event){
 
-    console.log(event.target.dataset);
-    //console.log(product);
     var id = event.target.dataset.param;
 
     return axios.get('http://localhost:4000/product/deleteProduct/' + id)
@@ -103,6 +103,8 @@ class ProductShop extends React.Component {
 
     /*var  photoIndex = 0;
     var  isOpen = false;*/
+    var isEditMode = true;
+
     return (
 
       <div className="container-fluid">
@@ -121,33 +123,119 @@ class ProductShop extends React.Component {
 
         </div>
 
+        
+
         <div className="row sectionGap productShopMainItems">
 
-          
-            
             {this.state.messages.map(product => (
               
+              
+
               <div className="row" key={product._id}>
+
                 <div className="col-sm-10" className="panelMargin">
-                  <div className={ product.ProductType == 'Used' ? 'productOldImageBorder' : 'productNewImageBorder' }>
+
+                  {isEditMode === true &&
+                    
+                    <Form  onSubmit={this.handleSubmit}>
+                      <div className="editFormBackGround">
+
+                      
+                        
+                      <br />
+                      
+                      <h3 className="formEditTitle"> Edit Existing Toy </h3>
+                      <br />
+                      <center><img src={require(`C:/Users/saif/ToyShop/assetSource/upload/${product.ProductUniqueId}.png`)} onClick={() => this.setState({ isOpen: true, selectedImageSrc: require(`C:/Users/saif/ToyShop/assetSource/upload/${product.ProductUniqueId}.png`) })} style={{height:200, width:400}} /> </center>
+
+
+                      <Form.Group as={Col} controlId="productFormTitle">
+                        <Form.Label>Product Title</Form.Label>
+                        <Form.Control />
+                      </Form.Group>
+
+                      
+
+                      <Form.Group as={Col} controlId="productFormPrice">
+                        <Form.Label> Product Price </Form.Label>
+                        <Form.Control value={product.ProductPrice} />
+                      </Form.Group>
+                      
+
+                      
+                        
+                      <Form.Group as={Col} controlId="productFormPassword">
+                        <Form.Label>Admin Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                      </Form.Group>
+                      
+
+                      
+                      <Form.Group as={Col} controlId="productFormUniqueId">
+                        <Form.Label>Product Id </Form.Label>
+                        <Form.Control disabled placeholder="" value={product.ProductUniqueId} />
+                      </Form.Group>
+
+                      <Form.Group as={Col} controlId="productFormOffer">
+                        <Form.Label>Product Offer </Form.Label>
+                        <Form.Control placeholder="" value={product.ProductOffer} />
+                      </Form.Group>
+
+                     
+
+                      <Form.Group as={Col}  controlId="productFormType">
+                        <Form.Label>Product Type</Form.Label>
+                        <Form.Control as="select">
+                          <option>Brand New</option>
+                          <option> Used </option>
+                        </Form.Control>
+                      </Form.Group>
+
+                      <Form.Group as={Col}  controlId="productFormProductDes">
+                        <Form.Label>Product Description </Form.Label>
+                        <Form.Control as="textarea" rows="3" value={product.ProductDes} />
+                      </Form.Group>
+
+
+                      <Form.Group  id="productFormConfirmBox">
+                        <Form.Check type="checkbox" label="Please Confirm Details Above Are Correct " />
+                      </Form.Group>
+                      <br />
+                      <Button className="inputStyle" variant="primary" type="submit">
+                        Update
+                      </Button>
+                      <br />
+                      <br />
+
+                    </div>
+                    </Form>
+                  }
+
+
+                  {isEditMode === false &&
+                    
+                    <div className={ product.ProductType == 'Used' ? 'productOldImageBorder' : 'productNewImageBorder' }>
                   
-                  <center><img src={require(`C:/Users/saif/ToyShop/assetSource/upload/${product.ProductUniqueId}.png`)} onClick={() => this.setState({ isOpen: true, selectedImageSrc: require(`C:/Users/saif/ToyShop/assetSource/upload/${product.ProductUniqueId}.png`) })} style={{height:200, width:200}} /> </center>
-                  < br/>
-                  <p> Description: {product.ProductDes} </p>
-                  <p> Src: {product.src} </p>
-                  <p> Price: £{product.ProductPrice} </p>
-                  <p> Type: {product.ProductType} </p>
-                  <p> Offer: {product.ProductOffer} </p>
-                  <p> Title: {product.ProductText} </p>
+                        <center><img src={require(`C:/Users/saif/ToyShop/assetSource/upload/${product.ProductUniqueId}.png`)} onClick={() => this.setState({ isOpen: true, selectedImageSrc: require(`C:/Users/saif/ToyShop/assetSource/upload/${product.ProductUniqueId}.png`) })} style={{height:200, width:400}} /> </center>
+                        < br/>
+                        <p> <b> Title: {product.ProductText} </b> </p>
+                        <p> <b> Type: {product.ProductType} </b> </p>
+                        <p> <b>Description: {product.ProductDes} </b> </p>
+                        <p> <b> Price: £{product.ProductPrice} </b> </p>
+                        <p className="offer"> <b> Offer: {product.ProductOffer} </b> </p>
+                        
 
-                  <input type="number" className="quantityHeight" id="quantity" placeholder="Qty" />
+                        <input type="number" className="quantityHeight" id="quantity" placeholder="Qty" />
 
-                  <button className="button btn btn-primary" onClick={this.addProduct}>Add To Basket </button>
-                  <button data-param={product._id} className="button btn btn-danger" onClick={this.deleteProduct}>Delete</button>
+                        <button className="button btn btn-primary" onClick={this.addProduct}>Add To Basket </button>
+                        <button data-param={product._id} className="button btn btn-danger" onClick={this.deleteProduct}>Delete</button>
 
-                  <button className="button btn btn-success" onClick={this.editProduct}>Edit</button>
-                </div>
-                </div>
+                        <button className="button btn btn-success" onClick={this.editProduct}>Edit</button>
+                      </div>
+
+                    }
+
+                  </div>
               </div>
               
             ))}
@@ -163,7 +251,7 @@ class ProductShop extends React.Component {
               
             />
           )}
-          </div>
+        </div>
 
       </div>
     )
